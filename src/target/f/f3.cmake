157a4d76002971ece7eec16a0df1e1416dd77964
Jacob Walser <jwalser90@gmail.com>
2019-08-16 00:26:35 -0400

    f*.cmake: use exported BOILERPLATE path if it exists
    this allows projects that embed this project to include this file

--- src/target/f/f3.cmake
+++ src/target/f/f3.cmake
@@ -1,24 +1,24 @@
 set(ADC_SRC
-  ${PROJECT_SOURCE_DIR}/lib/libopencm3/lib/stm32/common/adc_common_v2.c
-  ${PROJECT_SOURCE_DIR}/lib/libopencm3/lib/stm32/common/adc_common_v2_multi.c
-  ${PROJECT_SOURCE_DIR}/lib/libopencm3/lib/stm32/f3/adc.c
+  ${BOILERPLATE}/lib/libopencm3/lib/stm32/common/adc_common_v2.c
+  ${BOILERPLATE}/lib/libopencm3/lib/stm32/common/adc_common_v2_multi.c
+  ${BOILERPLATE}/lib/libopencm3/lib/stm32/f3/adc.c
 )
 
 set(DMA_SRC
-  ${PROJECT_SOURCE_DIR}/lib/libopencm3/lib/stm32/common/dma_common_l1f013.c
+  ${BOILERPLATE}/lib/libopencm3/lib/stm32/common/dma_common_l1f013.c
 )
 
 set(GPIO_SRC
-  ${PROJECT_SOURCE_DIR}/lib/libopencm3/lib/stm32/common/gpio_common_all.c
-  ${PROJECT_SOURCE_DIR}/lib/libopencm3/lib/stm32/common/gpio_common_f0234.c
+  ${BOILERPLATE}/lib/libopencm3/lib/stm32/common/gpio_common_all.c
+  ${BOILERPLATE}/lib/libopencm3/lib/stm32/common/gpio_common_f0234.c
 )
 
 set(RCC_SRC
-  ${PROJECT_SOURCE_DIR}/lib/libopencm3/lib/stm32/common/rcc_common_all.c
-  ${PROJECT_SOURCE_DIR}/lib/libopencm3/lib/stm32/common/flash_common_all.c
-  ${PROJECT_SOURCE_DIR}/lib/libopencm3/lib/stm32/f3/rcc.c
+  ${BOILERPLATE}/lib/libopencm3/lib/stm32/common/rcc_common_all.c
+  ${BOILERPLATE}/lib/libopencm3/lib/stm32/common/flash_common_all.c
+  ${BOILERPLATE}/lib/libopencm3/lib/stm32/f3/rcc.c
 )
 
 SET(TIMER_SRC
-  ${PROJECT_SOURCE_DIR}/lib/libopencm3/lib/stm32/common/timer_common_all.c
+  ${BOILERPLATE}/lib/libopencm3/lib/stm32/common/timer_common_all.c
 )
