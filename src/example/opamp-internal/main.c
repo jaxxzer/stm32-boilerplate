e49e65b0a988d54399a8283a6fb71d589b6b2210
Jacob Walser <jwalser90@gmail.com>
2019-12-28 21:16:31 -0500

    example/opamp-internal: cleanup/comment

--- src/example/opamp-internal/main.c
+++ src/example/opamp-internal/main.c
@@ -1,3 +1,13 @@
+/*
+ * this example uses the internal opamp1 in PGA mode, where
+ * the signal present on VINP0 (PA1) is amplified with gain=2
+ * the output signal is connected internally to the adc, and is
+ * disconnected to the gpio for the opamp output, so that gpio
+ * can be used for other puroposes. VINP0 (PA1) must be configured
+ * in analog mode (default configuration at reset, so we don't tend
+ * to that here)
+ */
+
 #include <target.h>
 
 #include <libopencm3/stm32/adc.h>
@@ -10,27 +20,14 @@ uint8_t channel_array[] = {ADC_OPAMP_CHANNEL};
 
 void led_setup();
 void adc_setup();
+void opamp_setup();
 
 int main() {
   initializeSystemClocks();
   led_setup();
-
-  rcc_periph_clock_enable(RCC_SYSCFG);
-
-  // select pga mode on inverting input
-  OPAMP_CSR(OPAMP1) |= OPAMP_CSR_VM_SEL_PGA;
-  // non inverting gain = 2
-  OPAMP1_CSR |= (0b00000 << OPAMP_CSR_PGA_GAIN_SHIFT);
-  // connect opamp vp to VINP0 signal
-  OPAMP1_CSR |= (0b00 << OPAMP_CSR_VP_SEL_SHIFT);
-  // connect the opamp internally
-  OPAMP1_CSR |= OPAMP_CSR_OPAINTOEN;
-  // enable high speed mode
-  OPAMP1_CSR |= OPAMP_CSR_OPAHSM;
-  // enable the opamp
-  OPAMP1_CSR |= 1;
-
+  opamp_setup();
   adc_setup();
+
   adc_start_conversion_regular(ADC_PERIPH);
 
   uint32_t count;
@@ -72,4 +69,21 @@ void led_setup() {
   rcc_periph_clock_enable(LED_GPIO_RCC);
   gpio_mode_setup(LED_GPIO_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_GPIO_PIN);
   gpio_set(LED_GPIO_PORT, LED_GPIO_PIN);
+}
+
+void opamp_setup() {
+  rcc_periph_clock_enable(RCC_SYSCFG);
+
+  // select pga mode on inverting input
+  OPAMP_CSR(OPAMP1) |= OPAMP_CSR_VM_SEL_PGA;
+  // non inverting gain = 2
+  OPAMP1_CSR |= (0b00000 << OPAMP_CSR_PGA_GAIN_SHIFT);
+  // connect opamp vp to VINP0 signal
+  OPAMP1_CSR |= (0b00 << OPAMP_CSR_VP_SEL_SHIFT);
+  // connect the opamp internally
+  OPAMP1_CSR |= OPAMP_CSR_OPAINTOEN;
+  // enable high speed mode
+  OPAMP1_CSR |= OPAMP_CSR_OPAHSM;
+  // enable the opamp
+  OPAMP1_CSR |= 1;
 }
